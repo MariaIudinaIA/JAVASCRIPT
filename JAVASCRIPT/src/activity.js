e6e46a0751f5e247bd71638c4a9eeac6917272be
MariaIudinaIA <146329419+MariaIudinaIA@users.noreply.github.com>
2024-04-14 14:02:46 +0300

    Update activity.js

--- JAVASCRIPT/src/activity.js
+++ JAVASCRIPT/src/activity.js
@@ -1,3 +1,8 @@
+/**
+ * Асинхронно получает случайную активность с внешнего API.
+ * @returns {Promise<string>} Текст случайной активности.
+ * @throws {Error} Ошибка при выполнении запроса или парсинге данных.
+ */
 async function getRandomActivity() {
     try {
         const response = await fetch('https://www.boredapi.com/api/activity/');
